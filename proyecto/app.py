f1acb79c20d300c623c791ae2bb535cfa95fc23f
dainbowqueenlml <liberatogarciaad@gmail.com>
2024-11-21 10:29:52 -0600

    Delete proyecto directory

--- proyecto/app.py
+++
@@ -1,4 +0,0 @@
-from myapp import app #app variable global del servidor 
-
-if __name__ == '__main__':
-    app.run()
--- proyecto/myapp/task/models.py
+++
@@ -1,6 +0,0 @@
-from myapp import db 
-#Creacion de los modelos
-class Task(db.Model):
-    __tablename__='task'
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(255))
