f7a9fa85087dc683201b0ef73b8c13508555bc3d
dainbowqueenlml <liberatogarciaad@gmail.com>
2024-11-21 10:30:48 -0600

    Creacion de tablas y operaciones tipo CRUD

--- myapp/task/controllers.py
+++ myapp/task/controllers.py
@@ -1,11 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from myapp.task import operationsCRUD
+
 
 taskRoute = Blueprint('task', __name__, url_prefix='/task') #conjunto de rutas definidas que van a estar dentro del modulo
 
+task_list = ['tasks 1', 'tasks 2', 'tasks 3']
 #Creacion de rutas en el modulo rutas, todas estas son rutas que definimos 
 @taskRoute.route('/') #aqui muestra todo el contenido de la tabla "task"
 def index():
-    return 'Index'
+     #print(operationsCRUD.getById(2).name)
+    #print(operationsCRUD.getAll()[1].name)
+    #print(operationsCRUD.delete(1))
+    print(operationsCRUD.pagination().items)
+    return render_template('dashboard/task/index.html', task=task_list)
 
 @taskRoute.route('/<int:id>') # solo muestra la que tenga este id previo seleccionado
 def show(id:int):
@@ -13,13 +20,23 @@ def show(id:int):
 
 @taskRoute.route('/delete/<int:id>')
 def delete(id:int):
-    #aqui podemos meter la funcion de borrar y posterior la pagina
-    return 'delete ' +str(id)
+   if id != None and id != "":
+        del task_list[id]
+        return redirect(url_for('task.index'))
 
 @taskRoute.route('/create', methods=('GET','POST'))#crear pagina que tenga un formulario, usar get y post sirven para enviar info, get para info x y post para info sensible
 def create():
-    return 'Create '
+    task = request.form.get('task')
+    if task != None and task != "":
+        task_list.append(task)
+        return redirect(url_for('task.index'))
+    return render_template('dashboard/task/create.html')
 
 @taskRoute.route('/update/<int:id>', methods=['GET','POST']) 
 def update(id:int):
-    return 'update '+str(id)
+    task = request.form.get('task')
+    if task != None and task != "":
+        task_list[id] = task
+        return redirect(url_for('task.index'))
+        
+    return render_template('dashboard/task/update.html')
